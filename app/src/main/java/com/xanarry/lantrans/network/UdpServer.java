6e7d4b327f807a181601f26babe5fc0f395e6091
Jinux <jinguol999@163.com>
2018-11-12 00:05:16 +0800

    change the udp server so it can chat with the python client

--- app/src/main/java/com/xanarry/lantrans/network/UdpServer.java
+++ app/src/main/java/com/xanarry/lantrans/network/UdpServer.java
@@ -31,19 +31,24 @@ public class UdpServer {
     public DatagramPacket waitClient() {
         //server wait and receive message broadcasted by client
         DatagramPacket packet = null;
-        byte[] recvBuf = new byte[Configuration.STRING_BUF_LEN];
+        String broadcastData = "I'm sender, where is the receiver";
+        byte[] recvBuf = new byte[broadcastData.getBytes().length];
         try {
             serverSockent = new DatagramSocket(port);//设置服务器端口, 监听广播信息
             serverSockent.setSoTimeout(Configuration.WAITING_TIME * 1000);//等待10秒
+
             DatagramPacket message = new DatagramPacket(recvBuf, recvBuf.length);
-
             serverSockent.receive(message);//接收client的广播信息
-            String strmsg = Utils.getMessage(message.getData());
-            message.setData((Configuration.currentTcpPort + Configuration.DELIMITER).getBytes("utf-8"));//将服务器的主机名发送给client
-            serverSockent.send(message);//回复信息tcp要使用的Tcp端口给client
 
-            message.setData(strmsg.getBytes("utf-8"));
-            packet = message;
+            String strmsg = new String(message.getData());
+            Log.e(TAG, "waitClient: " + strmsg);
+            if (broadcastData.equals(strmsg)) {
+                message.setData("I'm receiver and I'm here".getBytes());//将服务器的主机名发送给client
+                serverSockent.send(message);//回复信息tcp要使用的Tcp端口给client
+                Log.e(TAG, "waitClient: send out");
+                message.setData(strmsg.getBytes());
+                packet = message;
+            }
         } catch (SocketTimeoutException e) {
             progressListener.updateProgress(-3, 100, 100, 999);
             Log.e(TAG, "udp server 等待超时");
@@ -54,6 +59,7 @@ public class UdpServer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        serverSockent.close();
         return packet;//将client发送的数据包返回给调用者, 里面包含client的地址, 端口, 主机名
     }
 
