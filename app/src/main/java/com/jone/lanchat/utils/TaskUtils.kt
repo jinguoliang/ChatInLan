ef12e81eafe55168b45e36cc89d35cf49a45e06d
Jinux <jinguol999@163.com>
2018-11-24 23:13:11 +0800

    just log a message while it is in loop

--- app/src/main/java/com/jone/lanchat/utils/TaskUtils.kt
+++ app/src/main/java/com/jone/lanchat/utils/TaskUtils.kt
@@ -1,8 +1,11 @@
 package com.jone.lanchat.utils
 
+import com.empty.jinux.baselibaray.log.loge
+
 fun runUntil(timeout: Int, function: () -> Unit) {
     val startTime = System.currentTimeMillis()
     while (System.currentTimeMillis() - startTime < timeout) {
         function.invoke()
+        loge("looping", "runUntil")
     }
 }
